adbd642562b4831208a77eb09bd5829503b328a4
David Weinstock <dweinstock@frontsteps.com>
2019-10-07 21:44:57 -0600

    add floating navbar

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,12 @@
-const form = document.querySelector('#contact-form')
-const inputs = form.querySelectorAll('.input')
-const button = form.querySelector('.btn')
-
-form.addEventListener('input', () => button.disabled = !Array.from(inputs).every(input => input.value))
+document.addEventListener('scroll', () => {
+  const navbar = document.getElementById('navbar')
+  const bounding = navbar.getBoundingClientRect()
+  if (window.scrollY > bounding.height) {
+    navbar.classList.add('fixed')
+    document.querySelector('body').style.paddingTop = `${bounding.height}px`
+  }
+  else if (window.scrollY === 0) {
+    navbar.classList.remove('fixed')
+    document.querySelector('body').style.paddingTop = 0
+  }
+})
